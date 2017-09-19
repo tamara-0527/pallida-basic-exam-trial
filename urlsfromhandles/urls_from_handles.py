497d53539a705e982a3aa5da75d1c8a354ebc39d
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-19 12:59:45 +0200

    feat(urls) giving some features

--- urlsfromhandles/urls_from_handles.py
+++ urlsfromhandles/urls_from_handles.py
@@ -8,4 +8,13 @@
 # output: ["https://github.com/greenfox-academy/ghhandle1", "https://github.com/greenfox-academy/ghhandle2"]
 
 names = ["ghhandle1", "ghhandle2"]
+given_url = "https://github.com/greenfox-academy/teststudent"
+
+def urls_from_handles(names):
+    given_url1 = given_url.split('/')
+    print(given_url1.index("teststudent"))
+    names1 = given_url1.insert(3, names(0))
+    print(names1)
+    names2 = given_url1.insert(3, names(1))
+    print(names2)
 print(urls_from_handles(names))
