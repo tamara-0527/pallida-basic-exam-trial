dca4bfd33f3bf7b22c718c9492df6fbffd7c4f81
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-19 10:40:02 +0200

    feat(namefromemail.py) Creating function

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -5,4 +5,20 @@
 # example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
 # accents does not matter
 
-print(name_from_email("elek.viz@exam.com"))
+#print(name_from_email("elek.viz@exam.com"))
+
+def creating_email():
+    #email = input("Give me your email address (firstName.lastName@exam.com) ")
+    email = "firstName.lastName@exam.com"
+    name_from_email = email.split('.')
+    print(name_from_email)
+    first_name = name_from_email[0]
+    create_last_name = name_from_email[1].split('@')
+    print(create_last_name)
+    last_name = create_last_name[0]
+    name = first_name, last_name = last_name, first_name
+    print(name)
+
+creating_email()   
+
+    
